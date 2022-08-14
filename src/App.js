4b2e4f57f88100cce2dabaa9451378ff08437011
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-08-14 12:05:39 -0300

    1408

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import rocket_svg from './svg-components/rocket.svg';
+import bullet_svg from './svg-components/bullet.svg';
 import './App.css';
 import { useState } from 'react';
 import ViewPort from './ViewPort';
@@ -6,6 +7,13 @@ import ViewPort from './ViewPort';
 function App() {
 
   const [rocketRotation, setRocketRotation] = useState(90);
+  const [bullet,setBullet] = useState([]);
+
+  const GenericalBullet = {
+    velocity: [0,0],
+    position: [0,0],
+    size: 10
+  };
 
   const viewport = ViewPort();
 
@@ -32,12 +40,16 @@ function App() {
   };
 
   const shot = (e) => {
-    
+    let newBullet = {...GenericalBullet};
+    newBullet.position = [e.clientX,e.clientY];
+    setBullet((l) => {return bullet.concat([newBullet])})
+    console.log(bullet)
   }
 
   return (
     <div className="App" style={board_style} onMouseMove={rotation} onClick={shot}>
       <img src={rocket_svg} alt='' style={{ ...rocket_style, transform: 'rotate(' + rocketRotation + 'deg)' }} />
+      {bullet.map((e,i) => {return <img src={bullet_svg} style={{position: 'absolute', left: e.position[0] - board_style.left - e.size, top: e.position[1] - board_style.top - e.size, width: e.size, height: e.size}} alt='' key={i}/>})}
     </div>
   );
 }
