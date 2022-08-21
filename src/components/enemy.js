d7e4a964c7c500a69a460f1f27639b5ca7e47027
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-08-21 01:49:57 -0300

    21082022

--- src/components/enemy.js
+++ src/components/enemy.js
@@ -1,9 +1,5 @@
 import { Vector2 } from "../vector";
 
-export const genericalEnemy = {
-
-};
-
 export class EnemyClass extends Vector2{
     constructor(transform = {x: 0, y: 0}, size = 24, style={}, model = null, life = 1){
         super(transform);
